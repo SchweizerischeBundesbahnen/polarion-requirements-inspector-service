1f3a0346a31ce6ca6829c5eb238a462c0965aa69
u237713 <sebastian.scheidegger@sbb.ch>
2025-04-01 14:47:21 +0200

    chore: integrate system tests in github repository
    Added system tests and changed to pytest

--- tests/test_service.py
+++ tests/test_service.py
@@ -1,14 +1,12 @@
 import logging
-from unittest import TestCase
 
 from app.requirements_inspector_service import main
 
 
-class TestService(TestCase):
-    @classmethod
-    def setUpClass(cls):
-        logging.disable(logging.CRITICAL)
-
-    def test_sys_exit(self):
-        with self.assertRaises(SystemExit):
-            main(0, 0)
+def test_sys_exit():
+    logging.disable(logging.CRITICAL)
+    try:
+        main(0, 0)
+        raise AssertionError()
+    except SystemExit:
+        pass
