f773a30662a4d761016c247760f042055defcc24
Sebastian Scheidegger <sebastian.scheidegger@sbb.ch>
2025-04-16 08:49:17 +0200

    feat: use fastapi instead of flask (#98)

--- tests/test_controller.py
+++ tests/test_controller.py
@@ -7,18 +7,19 @@ import sys
 from typing import TYPE_CHECKING, NamedTuple
 
 import pytest
-from flask import Flask
+from fastapi import FastAPI
+from fastapi.testclient import TestClient
 from python_requirements_inspector.type_definitions import RequirementsInspectorResponseItem, WorkItem
 
 from app.requirements_inspector_controller import create_test_app
 
 if TYPE_CHECKING:
-    from app.type_definitions import VersionDto
+    from app.type_definitions import VersionSchema
 
 
 class TestParams(NamedTuple):
     __test__ = False
-    app: Flask
+    app: FastAPI
     python_version: str
     requirements_inspector_version: str
     requirements_inspector_service_version: str
@@ -36,8 +37,8 @@ def test_params():
 
 def test_version(test_params: TestParams):
     """_summary_"""
-    with test_params.app.test_client() as test_client:
-        response: VersionDto = test_client.get("/version").json
+    with TestClient(test_params.app) as test_client:
+        response: VersionSchema = test_client.get("/version").json()
         assert response.get("python") == test_params.python_version
         assert response.get("polarion_requirements_inspector") == test_params.requirements_inspector_version
         assert response.get("polarion_requirements_inspector_service") == test_params.requirements_inspector_service_version
@@ -45,11 +46,11 @@ def test_version(test_params: TestParams):
 
 def test_inspect_workitems_valid(test_params: TestParams):
     """_summary_"""
-    with test_params.app.test_client() as test_client:
+    with TestClient(test_params.app) as test_client:
         expected = RequirementsInspectorResponseItem(
             id="test", language="en", smellDescription="In TITLE missingProcessword: Title contains no process word\n", smellComplex=0, smellPassive=0, smellWeakword=0, smellComparative=0, missingProcessword=True
         )
-        response: list[RequirementsInspectorResponseItem] = test_client.post("/inspect/workitems", json=[WorkItem(id="test", title="test", description="test", language="en")]).json
+        response: list[RequirementsInspectorResponseItem] = test_client.post("/inspect/workitems", json=[WorkItem(id="test", title="test", description="test", language="en")]).json()
         assert [expected] == response
 
 
@@ -58,18 +59,31 @@ def test_inspect_workitems_request_size_limit(test_params: TestParams):
     work_item: WorkItem = WorkItem(title="example", description="example", language="en")
     n = (2 << 24) // sys.getsizeof(json.dumps(work_item))
     work_items = [work_item for _ in range(n)]
-    with test_params.app.test_client() as test_client:
+    with TestClient(test_params.app, raise_server_exceptions=False) as test_client:
         response = test_client.post("/inspect/workitems", json=work_items)
         assert response.status_code == 413
 
 
 def test_inspect_workitems_invalid_json(test_params: TestParams):
-    with test_params.app.test_client() as test_client:
+    with TestClient(test_params.app) as test_client:
         response = test_client.post("/inspect/workitems", data='[{"title":"example","description":"example","language":"en"]')
-        assert response.status_code == 400
+        assert response.status_code == 422
 
 
 def test_inspect_workitems_non_iterable_json(test_params: TestParams):
-    with test_params.app.test_client() as test_client:
+    with TestClient(test_params.app) as test_client:
         response = test_client.post("/inspect/workitems", json=1)
-        assert response.status_code == 500
+        assert response.status_code == 422
+
+
+def test_openapi_endpoint(test_params: TestParams):
+    with TestClient(test_params.app) as test_client:
+        response = test_client.get("/static/openapi.json")
+        assert b"/version" in response.content
+        assert b"/inspect/workitems" in response.content
+
+
+def test_docs_endpoint(test_params: TestParams):
+    with TestClient(test_params.app) as test_client:
+        response = test_client.get("/api/docs")
+        assert response.status_code == 200
